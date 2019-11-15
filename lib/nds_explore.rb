33521715e8f749060f11ac7ceb93da0dc305ae55
Max M <maxmcfadden88@gmail.com>
2019-11-15 00:09:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
-pp (directors_database)
+directors_database
 
 # Call the method directors_database to retrieve the NDS
 
