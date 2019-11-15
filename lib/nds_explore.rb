2cdca1c3768d541ad7308e7cc39a0cc4ea156f6e
Max M <maxmcfadden88@gmail.com>
2019-11-15 01:01:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -2,13 +2,14 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
 
+pp directors_database
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
 pp nds
 end
 
-directors_database
+
 
 def print_first_directors_movie_titles
 spiel_movies = directors_database[0][:movies]
